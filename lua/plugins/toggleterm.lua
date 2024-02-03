37e53ee05140a6084a1003934da7a30d41356999
Manh Long Nguyen <mhlong.nguyen@gmail.com>
2024-02-03 20:50:01 +0100

    update configs

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  -- amongst your other plugins
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      open_mapping = [[<c-\>]],
+    },
+    config = true,
+  },
+}
